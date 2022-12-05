893bab3b439f4a4a09256bf544326f05787a065a
Erik Rothman <git.erikcr@gmail.com>
2022-12-05 11:55:32 -0300

    Adiciona alguns comentarios

--- MediaAritimetica/Utilidades/UtilidadesAritimetica.cs
+++ MediaAritimetica/Utilidades/UtilidadesAritimetica.cs
@@ -6,6 +6,11 @@ namespace MediaAritimetica.Utilidades
     public static class UtilidadesAritimetica
     {
         
+        /// <summary>
+        /// Verifica se o numero é primo
+        /// </summary>
+        /// <param name="numero"> Numero inteiro (int) </param>
+        /// <returns>Retorna true se for, se não false</returns>
         public static bool NumeroEPrimo(int numero)
         {
             for (int i = 2; i <= numero/2; i++)
@@ -17,28 +22,30 @@ namespace MediaAritimetica.Utilidades
             }
             return true;
         }
-
+        /// <summary>
+        /// Verifica se numero é positivo
+        /// </summary>
+        /// <param name="numero"> Numero inteiro (int)</param>
+        /// <returns> Retorna true se sim, false se não </returns>
         public static bool NumeroEPositivo(int numero)
         {
             return (numero >= 0);
         }
-
+        /// <summary>
+        /// Verifica se tabela excede o limite de 100 elementos
+        /// </summary>
+        /// <param name="tabela"> Conjunto de elementos do tipo inteiro </param>
+        /// <returns> Retorna true se sim, false se não</returns>
         public static bool TabelaExcedeLimite(int[] tabela)
         {
             if (0 < tabela.Length || tabela.Length <= 100) return false;
             return true;
         }
-
-        public static double TabelaMediaAritimetica(int[] tabela)
-        {
-            double resultado = 0;
-            foreach (int numero in tabela)
-            {
-                resultado += numero;
-            }
-            resultado /= tabela.Length;
-            return resultado;
-        }
+        /// <summary>
+        /// Calcula media aritimética de todos numeros inteiros contidos em uma tabela exceto numeros negativos e primos
+        /// </summary>
+        /// <param name="Tabela">Tabela de numeros inteiros</param>
+        /// <returns>Media aritimetica</returns>
         public static double MediaAritmeticaGestal(int[] Tabela)
         {
             List<int> novaTabela = new List<int>(); //Gerar uma nova tabela
